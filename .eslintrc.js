7a85fb772ef976a09e2ffa2a9483b8903b2259d9
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2023-08-26 23:46:24 +0900

    🔧 Remove prettier/@typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
     "prettier",
-    "prettier/@typescript-eslint"
   ],
   env: { jest: true },
   plugins: ["@typescript-eslint", "prettier", "simple-import-sort"],
